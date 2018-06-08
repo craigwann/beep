492c681bb0f295cf10caa54953e663774d7f599b
craigwann <craigwann@me.com>
2018-06-08 11:39:12 -0700

    got number range working

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
 //--------BACK END------------
+var countTo = [];
 
-function countTo(number) {
-  var number = 5;
-  var count = [];
-
-  for (var i = 0; i <= number; i++) {
-    count.push(i);
-    return count;
-  }
-};
+// function countTo(number) {
+//   var number = 5;
+//   var count = [];
+//
+//   for (var i = 0; i <= number; i++) {
+//     count.push(i);
+//     return count;
+//   }
+// };
 
 
 //--------FRONT END------------
 $(document).ready(function() {
   $(".theForm").submit(function(event) {
     event.preventDefault();
-    var countTo = [];
     var inputString = $(".inputNumber").val();
     var input = parseInt($(".inputNumber").val());
 
-    for (var i = 0; i <= number; i++) {
-      count.push(i);
-      return count;
+    for (var i = 0; i <= input; i++) {
+      countTo.push(i);
     }
 
 
 
 
-    $(".result").text(countTo);
+    $(".result").text(countTo.join(" - "));
   });
 });
