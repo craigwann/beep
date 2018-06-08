9e9734fcca40d5eaaadc3efc46ef79aa846f47a0
craigwann <craigwann@me.com>
2018-06-08 15:43:29 -0700

    struggles with array mapping

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //--------BACK END------------
 var arrayOrig = [];
-var arrayFinal = [];
+
+
 
 function dave() {
   var answer = "I'm sorry, Dave. I'm afraid I can't do that."
@@ -37,16 +38,15 @@ $(document).ready(function() {
     var inputString = $(".inputNumber").val();
     var input = parseInt($(".inputNumber").val());
 //-----add to arrayOrig
+
     for (var i = 0; i <= input; i++) {
       arrayOrig.push(i);
-    };
+    }
 
     for (var i =0; i <= arrayOrig.legnth; i++) {
-      var first = arrayOrig[i];
-      alert("here");
+      var first = arrayOrig.shift();
 
-      arrayOrig.shift();
-      arrayFinal.push();
+      console.log("here");
 
 
       // if (arrayOrig.indexOf(i) % 3 === 0 ) {
