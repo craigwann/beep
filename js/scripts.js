a30bfa9e570c4dee53f7aac182fa39a636d58e3c
craigwann <craigwann@me.com>
2018-06-08 11:17:40 -0700

    add count function

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
+//--------BACK END------------
 
+function countTo(number) {
+  var number = 5;
+  var count = [];
+
+  for (var i = 0; i <= number; i++) {
+    count.push(i);
+    return count;
+  }
+};
 
 
 //--------FRONT END------------
 $(document).ready(function() {
   $(".theForm").submit(function(event) {
     event.preventDefault();
-    var array = [];
-    var inputString = $(".inputNumber").val()
+    var countTo = [];
+    var inputString = $(".inputNumber").val();
     var input = parseInt($(".inputNumber").val());
 
+    for (var i = 0; i <= number; i++) {
+      count.push(i);
+      return count;
+    }
 
 
 
 
-    $(".result").text(array.join(""));
+    $(".result").text(countTo);
   });
 });
