2bcfd6b9c635684652bdff821f540e821ea71d88
craigwann <craigwann@me.com>
2018-06-08 14:14:31 -0700

    add js array functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 //--------BACK END------------
-var countTo = [];
+var arrayOrig = [];
+var arrayFinal = [];
 
-// function countTo(number) {
+function dave() {
+  var answer = "I'm sorry, Dave. I'm afraid I can't do that."
+  return answer
+};
+// //-----------
+// function check(number) {
+//   return number >= 2;
+// }
+// function myFunction() {
+//   testArray.find(check);
+// };
+//------------
+// function arrayOrig(number) {
 //   var number = 5;
 //   var count = [];
 //
@@ -18,14 +31,21 @@ $(document).ready(function() {
     event.preventDefault();
     var inputString = $(".inputNumber").val();
     var input = parseInt($(".inputNumber").val());
-
+//-----add to arrayOrig
     for (var i = 0; i <= input; i++) {
-      countTo.push(i);
+      arrayOrig.push(i);
     }
+    for (var i =0; i <= arrayOrig.legnth; i++) {
+      var first = arrayOrig.shift()
+      arrayFinal.push(first);
+      // if (arrayOrig.indexOf(i) % 3 === 0 ) {
+      //   arrayFinal.push(dave());
+      // } else arrayFinal()
+    };
 
 
 
 
-    $(".result").text(countTo.join(" - "));
+    $(".result").text(arrayFinal.join(" - "));
   });
 });
